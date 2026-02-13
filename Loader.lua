48a39e076360fc8966e1145fab6d497d9bc9c1d1
chikalgaming212-lang <chikalgaming212@gmail.com>
2026-02-13 07:54:43 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,53 @@
+getgenv().Setting = {
+    Team = "Pirates",
+
+    ["Auto Click"] = true,
+    ["Delay Click"] = 0.3,
+
+    EnableWeapon = {
+        Fruit = false,
+        Melee = true,
+        Sword = true,
+        Gun = false
+    },
+
+    Melee = {
+        Z = {Enable = true, HoldTime = 0.1},
+        X = {Enable = true, HoldTime = 0.1},
+        C = {Enable = true, HoldTime = 0.1},
+        ChangeDelay = 0.1
+    },
+
+    Sword = {
+        Z = {Enable = true, HoldTime = 0.1},
+        X = {Enable = true, HoldTime = 0.1},
+        ChangeDelay = 0.2
+    },
+
+    Fruit = {
+        Z = {Enable = true, HoldTime = 0.1},
+        X = {Enable = true, HoldTime = 0.1},
+        C = {Enable = true, HoldTime = 0.1},
+        F = {Enable = true, HoldTime = 0.1},
+        ChangeDelay = 0.5
+    },
+
+    LockBounty = {
+        Enable = false,
+        Min = 0,
+        Max = 30000000
+    },
+
+    Setting = {
+        UseRaceV3 = true,
+        UseRaceV4 = true,
+        AutoServerHop = true,
+        ["Fast Attack"] = true
+    },
+
+    AimBot = {
+        Enable = true,
+        AimCamera = {Enable = true, MaxDistance = 300},
+        LockAim = {Enable = true}
+    }
+}
