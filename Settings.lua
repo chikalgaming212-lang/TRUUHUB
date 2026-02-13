f858fcb5e08eab1727708e76e714d683e895f5aa
chikalgaming212-lang <chikalgaming212@gmail.com>
2026-02-13 08:16:57 +0700

    Update Settings.lua

--- Settings.lua
+++ Settings.lua
@@ -1,54 +1,49 @@
 getgenv().Setting = {
     Team = "Pirates",
 
-    ["Auto Click"] = true,
-    ["Delay Click"] = 0.3,
+    AutoClick = true,
+    ClickDelay = 0.3,
 
-    EnableWeapon = {
-        Fruit = false,
+    Weapon = {
         Melee = true,
         Sword = true,
+        Fruit = false,
         Gun = false
     },
 
-    Melee = {
-        Z = {Enable = true, HoldTime = 0.1},
-        X = {Enable = true, HoldTime = 0.1},
-        C = {Enable = true, HoldTime = 0.1},
-        ChangeDelay = 0.1
-    },
-
-    Sword = {
-        Z = {Enable = true, HoldTime = 0.1},
-        X = {Enable = true, HoldTime = 0.1},
-        ChangeDelay = 0.2
-    },
-
-    Fruit = {
-        Z = {Enable = true, HoldTime = 0.1},
-        X = {Enable = true, HoldTime = 0.1},
-        C = {Enable = true, HoldTime = 0.1},
-        F = {Enable = true, HoldTime = 0.1},
-        ChangeDelay = 0.5
-    },
-
-    LockBounty = {
-        Enable = false,
+    Bounty = {
+        Enable = true,
         Min = 0,
-        Max = 30000000
-    },
-
-    Setting = {
-        UseRaceV3 = true,
-        UseRaceV4 = true,
-        AutoServerHop = true,
-        ["Fast Attack"] = true
+        Max = 30000000,
+        Blacklist = {"Admin","Tester"}
     },
 
     AimBot = {
         Enable = true,
-        AimCamera = {Enable = true, MaxDistance = 300},
-        LockAim = {Enable = true}
+        Distance = 350,
+        Smooth = 0.15,
+        Prediction = 0.135,
+        FOV = 120,
+        ShowFOV = true
+    },
+
+    Performance = {
+        AntiFreeze = true,
+        FPSBoost = true
+    },
+
+    Evade = {
+        Enable = true,
+        HealthPercent = 25
+    },
+
+    Cloud = {
+        Enable = true,
+        Url = "https://raw.githubusercontent.com/chikalgaming212-lang/TRUUHUB/main/cloud_settings.json"
+    },
+
+    ServerHop = {
+        Enable = true,
+        MinPlayers = 6
     }
 }
-
